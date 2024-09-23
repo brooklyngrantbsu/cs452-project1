ec54835df915de717936467df0e8fba81b1cb457
Brooklyn Grant <111612908+brooklyngrantbsu@users.noreply.github.com>
2024-09-23 04:12:49 +0000

    Task 3

--- app/main.c
+++ app/main.c
@@ -1,7 +1,35 @@
+#include <ctype.h>
 #include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include "../src/lab.h"
 
-int main(void)
+
+int main(int argc, char **argv)
 {
-  printf("hello world\n");
+
+  int c;
+
+  while ((c = getopt(argc, argv, "v")) != -1)
+    switch (c)
+    {
+      case 'v': 
+        printf("Shell version: %d.%d\n", lab_VERSION_MAJOR, lab_VERSION_MINOR);
+        exit(EXIT_SUCCESS);
+        break;
+        
+      case '?': // case outside a b or c
+        if (isprint(optopt))
+          fprintf(stderr, "Unknown option `-%c`.\n", optopt);
+        else
+          fprintf(stderr, "Unknown option character `\\x%x`.\n", optopt);
+        return 1;
+        break;
+
+      default:
+        abort();
+        break;
+    }
+
   return 0;
 }
