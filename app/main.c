69da861d65f6fc1ce8b60c7a53345836da428552
Brooklyn Grant <111612908+brooklyngrantbsu@users.noreply.github.com>
2024-09-23 21:26:31 +0000

    Task 6 - exit cd history

--- app/main.c
+++ app/main.c
@@ -5,7 +5,52 @@
 #include "../src/lab.h"
 #include <readline/readline.h>
 #include <readline/history.h>
+#include <pwd.h>
+#include <sys/types.h>
+#include <string.h>
 
+// Function to change directory
+void change_directory(char *path) {
+    char *home = getenv("HOME");
+
+    if (path == NULL || strcmp(path, "") == 0) {
+        printf("No directory specified. Switching to HOME directory.\n");
+        if (home == NULL) { // home note set, set it manually with user info
+            struct passwd *pw = getpwuid(getuid());
+            if (pw != NULL) {
+                home = pw->pw_dir; 
+            } else {
+                printf("Error: cannot find home directory\n");
+                return;
+            }
+        }
+        int ret = chdir(home); // go to directory since not NULL anymore
+        if (ret != 0) {
+            printf("Error changing directory\n");
+        } else {
+            printf("Successfully changed directory to: '%s'\n", home);
+        }
+    } else {
+        int ret = chdir(path);
+        if (ret != 0) {
+            printf("Error changing directory\n");
+        } else {
+            printf("Successfully changed directory to: '%s'\n", path);
+        }
+    }
+}
+
+// Function to print command history
+void print_history() {
+    printf("Command History: \n");
+
+    HIST_ENTRY **historyList = history_list();
+    if (historyList != NULL) {
+        for (int i = 0; historyList[i] != NULL; i++) {
+            printf("%d: %s\n", i + 1, historyList[i]->line);
+        }
+    }
+}
 
 int main(int argc, char **argv)
 {
@@ -40,13 +85,41 @@ int main(int argc, char **argv)
   char *prompt = getenv("MY_PROMPT"); // check if env variable exists
   
   if (prompt == NULL) {
-      prompt = "$"; // default if null
+      prompt = "$> "; // default if null
   }
 
   while ((line=readline(prompt))){
-      printf("%s\n",line);
+    if (*line) {
       add_history(line);
-      free(line);
+
+      char *args[3];  // max 3 args
+      int i = 0;
+      char *token = strtok(line, " ");
+      while (token != NULL && i < 2) {
+          args[i++] = token;
+          token = strtok(NULL, " ");
+      }
+      args[i] = NULL;
+
+      if (strcmp(args[0], "exit") == 0) {
+        exit(0);  // exit with status 0
+      } else if (strcmp(args[0], "cd") == 0) {
+          change_directory(args[1]);  // change directory
+          free(line);
+          continue; 
+      } else if (strcmp(args[0], "history") == 0) {
+          print_history();  // print the cmd history
+          free(line);
+          continue; 
+      } else {
+          fprintf(stderr, "Not a valid command.\n");
+          free(line);
+          continue; 
+      }
+
+    }
+
+    free(line);
   }
 
   return 0;
